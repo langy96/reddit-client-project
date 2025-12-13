2bf77cfba022e41e49721485d3e2054dd0fe0b9b
langy <jdlang831@gmail.com>
2025-12-13 19:26:15 +0000

    Further CSS amendments to improve Subreddit tab text visibility

--- src/features/subreddits/SubredditPicker.jsx
+++ src/features/subreddits/SubredditPicker.jsx
@@ -1,23 +1,20 @@
-// src/features/subreddits/SubredditPicker.jsx
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrent } from './subredditsSlice';
 import { loadPostsBySubreddit } from '../posts/postsSlice';
 
 export default function SubredditPicker() {
   const dispatch = useDispatch();
-  const { options, current } = useSelector(s => s.subreddits);
+  const { options, current } = useSelector((s) => s.subreddits);
 
   return (
     <div style={{ padding: 16, display: 'flex', gap: 8, flexWrap: 'wrap' }}>
-      {options.map(opt => (
+      {options.map((opt) => (
         <button
           key={opt}
-          onClick={() => { dispatch(setCurrent(opt)); dispatch(loadPostsBySubreddit(opt)); }}
-          style={{
-            padding: '6px 10px',
-            border: '1px solid #ccc',
-            borderRadius: 999,
-            background: current === opt ? '#eef' : '#fff'
+          className={`subreddit-button ${current === opt ? 'active' : ''}`}
+          onClick={() => {
+            dispatch(setCurrent(opt));
+            dispatch(loadPostsBySubreddit(opt));
           }}
         >
           r/{opt}
